6936152554b20fb3929c661356c740d78896f86f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 09:17:45 +0530

    export all the funciton. using export

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,15 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
 
 export function diff(a, b) {
   return a - b;
+}
+
+export function mul(a, b) {
+  return a * b;
+}
+
+export function div(a, b) {
+  return a / b;
 }
