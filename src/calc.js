a59d8454756dbc3d579a33de6064fc04c133565e
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 09:15:14 +0530

    git remote add origin https://github.com/dac-2022/day16-js.git

---
+++ src/calc.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+
+export function diff(a, b) {
+  return a - b;
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+let calc = require("./calc");
+
+const output = calc.sum(1, 1);
+console.log(output);
